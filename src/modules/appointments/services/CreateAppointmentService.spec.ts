22caeb838f3ff2611e37db5595cec84fb228ea61
tsunoda1 <tsunoda@b2ml.com.br>
2020-09-21 21:59:22 -0300

    feat: create firt test using JEST (TDD)

---
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,24 @@
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '123131561',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('123131561');
+  });
+
+  //   it('should not be able to create to appointments on the same time', () => {
+  //       expect
+  //   });
+});
